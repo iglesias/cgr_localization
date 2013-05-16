84665d3a4ccf696d4be14334c67c5eb45fef8219
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2013-05-16 16:02:35 +0200

    Fix DOC typos

--- src/cgr/vectorparticlefilter.h
+++ src/cgr/vectorparticlefilter.h
@@ -232,9 +232,9 @@ public:
   void getPointCloudGradient(vector2f loc, float angle, vector2f& locGrad, float& angleGrad, const std::vector< vector2f >& pointCloud, const std::vector< vector2f >& pointNormals, float& logWeight, const VectorLocalization2D::PointCloudParams& pointCloudParams, const std::vector< int >& lineCorrespondences, const std::vector< line2f >& lines);
   /// Gradient based on LIDAR observation
   void getLidarGradient(vector2f loc, float angle, vector2f& locGrad, float& angleGrad, float& logWeight, VectorLocalization2D::LidarParams lidarParams, const vector< Vector2f >& laserPoints, const vector<int> & lineCorrespondences, const vector<line2f> &lines);
-  /// Observation likelihood based on LIDAR obhservation
+  /// Observation likelihood based on LIDAR observation
   float observationWeightLidar(vector2f loc, float angle, const VectorLocalization2D::LidarParams& lidarParams, const std::vector< Vector2f >& laserPoints);
-  /// Observation likelihood based on point cloud obhservation
+  /// Observation likelihood based on point cloud observation
   float observationWeightPointCloud(vector2f loc, float angle, vector< vector2f >& pointCloud, vector< vector2f >& pointNormals, const PointCloudParams& pointCloudParams);
   /// Probability of specified pose corresponding to motion model
   float motionModelWeight(vector2f loc, float angle, const VectorLocalization2D::MotionModelParams& motionParams);
